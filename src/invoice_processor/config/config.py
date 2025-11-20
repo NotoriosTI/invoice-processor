3f2edb942e57cd6d08d969dc4f84e0e6fb4a4e0e
Tomás Núñez <tomasnm2003@gmail.com>
2025-11-20 14:30:29 -0300

    Prototipo 1.0

--- src/invoice_processor/config/config.py
+++ src/invoice_processor/config/config.py
@@ -1,17 +1,24 @@
 from pydantic_settings import BaseSettings
 from functools import lru_cache
+from env_manager import init_config, required_config, get_config
 
-class Settings(BaseSettings):
-    odoo_url: str
-    odoo_db: str
-    odoo_username: str
-    odoo_password: str
-    openai_api_key: str
-    openai_model: str = "gpt-4o-mini"
+init_config(
+    "config/config_vars.yaml",
+    secret_origin=None, 
+    gcp_project_id=None,
+    strict=None,
+    dotenv_path=None,
+    debug=False,
+)
 
-    class config:
-        env_file = ".env"
-        case_sensitive = True
+class Settings(self):
+    self.odoo_url: str = required_config("ODOO_URL")
+    self.odoo_db: str = required_config("ODOO_DB")
+    self.odoo_username: str = required_config("ODOO_USERNAME")
+    self.odoo_password: str = required_config("ODOO_PASSWORD")
+    self.openai_api_key: str = required_config("OPENAI_API_KEY")
+    self.openai_model: str = get_config("OPENAI_MODEL", default="gpt-4o-mini")
+    
 
 @lru_cache
 def get_settings(): -> Settings:
